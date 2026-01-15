641a744c269b3866ddaa2f686857a24a082f7de1
leila-wilde <leila.wilde@laplateforme.io>
2026-01-15 12:20:35 +0000

    feat: add fork() and evecve() to execute_external() function to create child process and replace to process image with a new one and load the new program. Also add waitpid() to wait for the child process to finish.

--- src/nanoshell.c
+++ src/nanoshell.c
@@ -48,6 +48,8 @@ void split_arguments(char *input, char *arguments[]) {
 
 // function to execute commands 
 int execute_external(char *arguments[]) {
+    pid_t pid = -1;
+
     // check if no command was entered 
     if (arguments[0] == NULL) {
         return 1;
@@ -57,9 +59,26 @@ int execute_external(char *arguments[]) {
     if (strcasecmp(arguments[0], "exit") == 0) {
         return 0; 
     }
+    // check for built-in commands
+    // todo
 
-    // just print the command for now
-    printf("Vous avez entre: %s", arguments[0]);
+    // find the command using the path
+    char *path = find_in_path(arguments[0]);
+    if (path == NULL) {
+        printf("Commande non trouvé: %s\n", arguments[0]);
+        return 1;
+    }
+    
+    pid = fork();
+    if (pid == 0) {
+        // child process
+        char *env[] = {NULL}; // null-terminated list of environment variables
+        execve(path, arguments, env);
+        exit(-1);
+    }
+
+    // parent process (wait for child process to finish)
+    waitpid(pid, NULL, 0);
 
     return 1;
 }
